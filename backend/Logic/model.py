5e001d844e60c9672f869416d1295de00746e40d
arose13 <anthony_rose12@hotmail.com>
2016-02-06 14:21:50 -0500

    cleaned up less retarded models

--- backend/Logic/model.py
+++ backend/Logic/model.py
@@ -1,36 +1,24 @@
 import numpy as np
-from collections import namedtuple
-
-Debt = namedtuple('Debt', ['title', 'amount', 'interest'])
+from backend.Logic.fakes import assets
 
 
-def interest_model(values, rates, months):
-    """
-    Compounding Interest
-    V(t) = Vo (1 + r) ^ t
+def pull_loan_data():
+    amount_array = []
+    interest_array = []
 
-    :param values: Array of values
-    :param rates: Monthly Interest Rates in Decimal (10% => 0.10/12)
-    :param months: Number of compounding months
-    :return: Progression of Balance
+    for item in assets:
+        amount_array.append(item.amount)
+        interest_array.append(item.interest)
 
-    >>> interest_model(200000, 0.10/12, months = 0)
-    200000.0
-    #TODO fix decimal place
-
-    '>>> interest_model(12000, 0.10/12, months = 14)
-    11921.52 (This calculation is for mortage)
-
-    """
-    return values * (1 + rates) ** months
+    amount_array = np.array(amount_array)
+    interest_array = np.array(interest_array)
+    return amount_array, interest_array
 
 
 def interest_rate_dx(values, rates, month):
-    """
-    V(t) = ( Vo ( 1 + r ) ^ t ) * ( log( r + 1 ) )
-    #TODO Verify the derivative on wolfram
-    """
+    # V(t) = ( Vo ( 1 + r ) ^ t ) * ( log( r + 1 ) )
     return (values * (rates + 1) ** month) * (np.log(rates + 1))
+
 
 def interest_step(principal, rate, period):
     """
@@ -45,11 +33,11 @@ def interest_step(principal, rate, period):
     100
     """
 
-    return principal*rate
+    return principal * rate
+
 
 def credit_time_to_pay(balance, rate, payment):
     """
-
     http://itools-ioutils.fcac-acfc.gc.ca/CCPC-CPCC/CCPCCalc-CPCCCalc-eng.aspx
     :param balance: credit card balance
     :param rate: monthly interest rate
@@ -71,32 +59,22 @@ def credit_time_to_pay(balance, rate, payment):
 
     return -1
 
+
 def credit_interest_paid(balance, rate, payment):
     """
-
-    :param balance: credit card balance
-    :param rate: monthly interest rate
-    :param payment: monthly payment
-    :return: number of compounding periods (months)
-
-    #TODO: can implement min payment as $10 or 2%
-
     >>> credit_time_to_pay(1000, 0.18/12, 20)
     862.24
-
     >>> credit_time_to_pay(1000, 0.18/12, 25)
     538.62
-
     >>> credit_time_to_pay(1000, 0.18/12, 100)
     91.62
-
     """
-
+    # TODO can implement min payment as $10 or 2%
     return -1
+
 
 def credit_total_paid(balance, rate, payment):
     """
-
     :param balance: credit card balance
     :param rate: monthly interest rate
     :param payment: monthly payment
@@ -114,22 +92,4 @@ def credit_total_paid(balance, rate, payment):
     1091.62
 
     """
-
     return -1
-
-if __name__ == '__main__':
-    # Testing
-    fake_data_array = [Debt(title='Student Loan', amount=12000, interest=0.02),
-                       Debt(title='Credit Card', amount=2000, interest=0.18),
-                       Debt(title='Credit Line', amount=5000, interest=0.08)]
-
-    amount_array = []
-    interest_array = []
-    for item in fake_data_array:
-        amount_array.append(item.amount)
-        interest_array.append(item.interest)
-
-    amount_array = np.array(amount_array)
-    interest_array = np.array(interest_array)
-
-    print(interest_model(amount_array, interest_array, 1))
