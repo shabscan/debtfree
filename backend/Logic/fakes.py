02be212668cbb50a606d2479cec9412b72a07717
arose13 <anthony_rose12@hotmail.com>
2016-02-06 17:36:58 -0500

    Real fake historical data is awesome awful. I couldn't lie I couldnt lie I couldn't lie!

--- backend/Logic/fakes.py
+++ backend/Logic/fakes.py
@@ -4,9 +4,18 @@ Debt = namedtuple('Debt', ['title', 'amount', 'interest'])
 HistoricalBalance = namedtuple('HistoricalBalance', ['week', 'balance'])
 
 balance_history = [
-    HistoricalBalance(week=10, balance=20300),
-    HistoricalBalance(week=11, balance=20050),
-    HistoricalBalance(week=12, balance=19900)
+    HistoricalBalance(week=1, balance=23529.29),
+    HistoricalBalance(week=2, balance=23157.58),
+    HistoricalBalance(week=3, balance=22784.24),
+    HistoricalBalance(week=4, balance=22409.27),
+    HistoricalBalance(week=5, balance=22032.66),
+    HistoricalBalance(week=6, balance=21654.41),
+    HistoricalBalance(week=7, balance=21274.50),
+    HistoricalBalance(week=8, balance=20892.92),
+    HistoricalBalance(week=9, balance=20509.68),
+    HistoricalBalance(week=10, balance=20124.76),
+    HistoricalBalance(week=11, balance=19738.15),
+    HistoricalBalance(week=12, balance=19349.86)
 ]
 
 assets = [
