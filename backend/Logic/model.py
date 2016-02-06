a9c1de1304c7d4eab709f6f963b24aa6cc78e04a
arose13 <anthony_rose12@hotmail.com>
2016-02-05 23:22:30 -0500

    doctest example

--- backend/Logic/model.py
+++ backend/Logic/model.py
@@ -2,9 +2,26 @@ import numpy as np
 
 
 def interest_model(values, rates, months):
-    # V(t) = Vo e ^ (r*t)
+    """
+    V(t) = Vo e ^ (r * t)
+    """
     return np.sum(values * np.e ** (rates * months))
 
 
 def interest_rate_dx(values, rates):
+    """
+    V(t) = Vo r e ^ (r * t)
+    """
     return values * rates * np.e ** rates
+
+
+def sum(a, b):
+    """
+    >>> sum(2, 3)
+    5
+    """
+    return a+b
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
