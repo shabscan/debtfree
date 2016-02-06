f2e2daf0cd9066437ed191ea4ea3c79e16df23c5
arose13 <anthony_rose12@hotmail.com>
2016-02-06 14:21:05 -0500

    Simulated Data

---
+++ backend/Logic/fakes.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+
+Debt = namedtuple('Debt', ['title', 'amount', 'interest'])
+HistoricalBalance = namedtuple('HistoricalBalance', ['week', 'balance'])
+
+balance_history = [
+    HistoricalBalance(week=10, balance=20300),
+    HistoricalBalance(week=11, balance=20050),
+    HistoricalBalance(week=12, balance=19900)
+]
+
+assets = [
+    Debt(title='Other', amount=5000, interest=0.12),
+    Debt(title='Credit Card', amount=2000, interest=0.18),
+    Debt(title='Student Loan', amount=12000, interest=0.01)
+]
