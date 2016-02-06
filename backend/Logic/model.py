971cf22debacb56e245b71338fc762371392a940
Kajendra Seevananthan <kajendra+git@gmail.com>
2016-02-06 13:15:06 -0500

    Added monthly payment calculator money format

--- backend/Logic/model.py
+++ backend/Logic/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from collections import namedtuple
 
 Debt = namedtuple('Debt', ['title', 'amount', 'interest'])
@@ -22,30 +23,37 @@ def interest_model(values, rates, months):
     11921.52 (This calculation is for mortage)
 
     """
-    return values * (1 + rates) ** months
+
+    return round(values * (1 + rates) ** months, 2)
 
 
 def interest_rate_dx(values, rates, month):
     """
     V(t) = ( Vo ( 1 + r ) ^ t ) * ( log( r + 1 ) )
     #TODO Verify the derivative on wolfram
+    :param values:
+    :param rates:
+    :param month:
+    :return:
     """
+
     return (values * (rates + 1) ** month) * (np.log(rates + 1))
+
 
 def interest_step(principal, rate, period):
     """
     Amount of interest over one compounding period
     Not very useful (alone) for now...
     :param principal: Starting Principal
-    :param rate: fractional montly rate 10% => 0.10/12
+    :param rate: fractional monthly rate 10% => 0.10/12
     :param period: number of months
     :return: Amount of interest
 
     >>> interest_step(10000, 0.01, 1)
-    100
+    100.00
     """
+    return format(principal*rate, ',.2f')
 
-    return principal*rate
 
 def credit_time_to_pay(balance, rate, payment):
     """
@@ -71,6 +79,7 @@ def credit_time_to_pay(balance, rate, payment):
 
     return -1
 
+
 def credit_interest_paid(balance, rate, payment):
     """
 
@@ -94,6 +103,7 @@ def credit_interest_paid(balance, rate, payment):
 
     return -1
 
+
 def credit_total_paid(balance, rate, payment):
     """
 
@@ -116,6 +126,48 @@ def credit_total_paid(balance, rate, payment):
     """
 
     return -1
+
+
+def format_money(amount):
+
+    """
+    Converts 123.234 to 123.23
+
+    :param amount:
+    :return:
+
+    >>>format_money(123.234)
+    123.23
+
+    >>format_money(100.0000)
+    100.00
+    """
+    # return round(payment, 2)
+    return format(amount, ',.2f')
+
+
+def calc_payment(balance, months, rate):
+    """
+    Calculates the monthly payment
+    if fed a weekly rate and number of compounding weeks, might calc weekly payment
+    #TODO verify weekly payment size
+
+    :param balance: credit card balance
+    :param months: number of compounding periods
+    :param rate: monthly interest rate
+    :return: monthly payments
+
+    >>> calc_payment(25000, 60, 5.25/100/12)
+    474.65
+    """
+
+    if rate == 0:
+        payment = balance / months
+    else:
+        compounded_rate = (rate + 1)**months
+        payment = balance * ((rate * compounded_rate) / (compounded_rate - 1))
+
+    return round(payment, 2)
 
 if __name__ == '__main__':
     # Testing
