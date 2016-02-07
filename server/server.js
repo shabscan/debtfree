13e1a180bdbab01c631f5ac31c448e575cf27b8b
Paul Xu <paulxu@Pauls-MacBook-Air.local>
2016-02-06 23:37:18 -0500

    user profile

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 Meteor.publish('userData', function () {
 return Meteor.users.find({_id: this.userId},
-{fields: {services: 1 }
+{fields: {services: 1,debtprofile:1, goalprofile:1, userHistory:1 }
 });
 });
 
