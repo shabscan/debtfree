fd43db92a9a7cac880b8c1bde975ad5563226f88
Kajendra Seevananthan <kajendra+git@gmail.com>
2016-02-07 00:14:53 -0500

    Got rid of functions that we could accomplish from libraries

--- backend/Logic/model.py
+++ backend/Logic/model.py
@@ -47,74 +47,6 @@ def interest_step(principal, rate, period):
     return format(principal * rate, ',.2f')
 
 
-def credit_time_to_pay(balance, rate, payment):
-    """
-
-    http://itools-ioutils.fcac-acfc.gc.ca/CCPC-CPCC/CCPCCalc-CPCCCalc-eng.aspx
-    #TODO: can implement min payment as $10 or 2%
-
-    >>> credit_time_to_pay(1000, 0.18/12, 20)
-    94
-
-    >>> credit_time_to_pay(1000, 0.18/12, 25)
-    62
-
-    >>> credit_time_to_pay(1000, 0.18/12, 100)
-    11
-
-    """
-
-    return -1
-
-
-def credit_interest_paid(balance, rate, payment):
-    """
-
-    :param balance: credit card balance
-    :param rate: monthly interest rate
-    :param payment: monthly payment
-    :return: number of compounding periods (months)
-
-    #TODO: can implement min payment as $10 or 2%
-
-    >>> credit_interest_paid(1000, 0.18/12, 20)
-    862.24
-
-    >>> credit_interest_paid(1000, 0.18/12, 25)
-    538.62
-
-    >>> credit_interest_paid(1000, 0.18/12, 100)
-    91.62
-
-    """
-    # TODO can implement min payment as $10 or 2%
-    return -1
-
-
-def credit_total_paid(balance, rate, payment):
-    """
-
-    :param balance: credit card balance
-    :param rate: monthly interest rate
-    :param payment: monthly payment
-    :return: number of compounding periods (months)
-
-    #TODO: can implement min payment as $10 or 2%
-
-    >>> credit_total_paid(1000, 0.18/12, 20)
-    1862.24
-
-    >>> credit_total_paid(1000, 0.18/12, 25)
-    1538.62
-
-    >>> credit_total_paid(1000, 0.18/12, 100)
-    1091.62
-
-    """
-
-    return -1
-
-
 def format_money(amount):
     """
     Converts 123.234 to 123.23
@@ -130,31 +62,6 @@ def format_money(amount):
     """
     # return round(payment, 2)
     return format(amount, ',.2f')
-
-
-def calc_payment(balance, months, rate):
-    """
-    Calculates the monthly payment
-    if fed a weekly rate and number of compounding weeks, might calc weekly payment
-    #TODO verify weekly payment size
-
-    :param balance: credit card balance
-    :param months: number of compounding periods
-    :param rate: monthly interest rate
-    :return: monthly payments
-
-    >>> calc_payment(25000, 60, 5.25/100/12)
-    474.65
-    """
-
-    if rate == 0:
-        payment = balance / months
-    else:
-        compounded_rate = (rate + 1) ** months
-        payment = balance * ((rate * compounded_rate) / (compounded_rate - 1))
-
-    alter_calc = np.pmt(rate, months, balance, 0)  # Gives a negative value
-    return format_money(payment, 2)
 
 if __name__ == '__main__':
     weighted_interest_sum()
